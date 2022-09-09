fe3d8d2320ef34eea29208b4b1c7df29880b444d
TaraHelberg <taralhelberg@outlook.com>
2022-09-09 08:28:18 +0000

    Add database boiler plate to system

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,6 +3,7 @@ Flask import to render templates & taskmanager import app
 """
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
